a4a2a334731e430a24fb13a5999b683d5c488bf0
alexwox <alexander.woxstrom@gmail.com>
2025-05-29 23:40:32 +0200

    Create generic for the handlers and types in types.d.ts

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,3 +1,12 @@
+import { ipcMain } from "electron";
+
 export function isDev(): boolean {
   return process.env.NODE_ENV === "development";
+}
+
+function ipcHandle<Key extends keyof EventPayloadMapping>(
+  key: Key,
+  handler: () => EventPayloadMapping[Key]
+) {
+  ipcMain.handle(key, () => handler());
 }
