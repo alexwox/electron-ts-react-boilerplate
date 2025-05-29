b17a3eef740ccb831422551811192a3658f86abf
alexwox <alexander.woxstrom@gmail.com>
2025-05-29 23:46:52 +0200

    Export the IPC handle function

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -4,7 +4,7 @@ export function isDev(): boolean {
   return process.env.NODE_ENV === "development";
 }
 
-function ipcHandle<Key extends keyof EventPayloadMapping>(
+export function ipcHandle<Key extends keyof EventPayloadMapping>(
   //Use this wrapper and never ipcMain.handle() directly.
 
   key: Key,
